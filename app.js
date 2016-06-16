92f973e067234e55aadd8d5cc7c3b472bce03758
adrianmarino <adrianmarino@gmail.com>
2016-06-15 23:57:33 -0300

    improve declarativity

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 // Requires
 //-----------------------------------------------------------------------------
 var server  = require("./lib/server");
-var router  = require("./lib/router");
+var routers = require("./lib/router");
 var common  = require("./lib/common");
 var view    = require("./view");
 var process = require("child_process");
@@ -17,33 +17,33 @@ var logger = common.Logger();
 //-----------------------------------------------------------------------------
 // Routes
 //-----------------------------------------------------------------------------
-router.handle("get", "/find", function(request, response) {
+routers.add("get", "/find", function(request, response) {
     process.exec("find /", { timeout: 50000, maxBuffer: 20000*1024 }, function (error, stdout, stderr) {
         response.write(stdout);
         response.end();
     });
 });
 
-router.handle("get", "/tree", function(request, response) {
+routers.add("get", "/tree", function(request, response) {
     process.exec("tree", function (error, stdout, stderr) {
         response.write(stdout);
         response.end();
     });
 });
 
-router.handle("get", "/sleep", function(request, response) {
+routers.add("get", "/sleep", function(request, response) {
     var timeout = request.param("timeout") || 500;
     logger.info("Start sleep by %s millis...", timeout);
     response.write("Timeout ends!");
     setInterval(function() { response.end(); }, timeout);
 });
 
-router.handle("get", "/index", function(request, response) {
+routers.add("get", "/index", function(request, response) {
     response.write(view.create_note({action: "/note"}));
     response.end();
 });
 
-router.handle("post", "/note", function(request, response) {
+routers.add("post", "/note", function(request, response) {
     request.get_fields("utf8", function(fields) {
         response.writeJson(fields);
         logger.info(JSON.stringify(fields));
@@ -54,5 +54,5 @@ router.handle("post", "/note", function(request, response) {
 //-----------------------------------------------------------------------------
 // Main
 //-----------------------------------------------------------------------------
-server.run(router);
+server.executeUsing(routers);
 common.exist_message("Bye Bye!");
